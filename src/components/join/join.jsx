eb84532a109c8ea5016d641e9f0f66946a88cede
sungmin-choi <namja306@naver.com>
2021-09-24 21:45:04 +0900

    join page

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
-
+import styles from './join.module.css';
 const Join = (props) => {
     return(
-        <h1>Join good</h1>
+        <section className={styles.body}>
+            <form className={styles.container}>
+                <input className={styles.input} type="text" placeholder="First Name" />
+                <input className={styles.input} type="text" placeholder="Last Name" />
+                <input className={styles.input} type="email" placeholder="Email" />
+                <input className={styles.input} type="text" placeholder="ID" />
+                <input className={styles.input} type="password" placeholder="Password" />
+                <input className={styles.input} type="password" placeholder="Check Password" />
+                <button className={styles.joinBtn}>Sign Up</button>
+
+                <div className={styles.alert}>Already have an account? <span className={styles.SignIn}>Sign in</span></div>
+            </form>
+            
+        </section>
+
+        
     )
 }
 
--- src/components/userAuth/userAuth.jsx
+++ src/components/userAuth/userAuth.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './userAuth.module.css';
 import Join from '../join/join';
 import Login from '../login/login';
@@ -6,16 +6,19 @@ const UserAuth = (props) => {
     const [select,setSelect] = useState("login");
     let description;
     if(select==="login") description = <Login/>;
-    else if(select==="join") description =<Join/>;
-
+    else if(select==="join") {
+        description =<Join/>;
+    }
     return (
+        <div className={styles.body}>
         <section className={styles.container}>
             <div className={styles.selectAuth}>
-                <div onClick={()=>{setSelect("join")}} className={styles.seletJoin}>Join</div>
+                <div onClick={()=>{setSelect("join")}} className={styles.seletJoin}>Sign Up</div>
                 <div onClick={()=>{setSelect("login")}} className={styles.seletLogin}>Login</div>
             </div>
             <section className={styles.main}>{description}</section>
         </section>
+        </div>
     )
 }
 
